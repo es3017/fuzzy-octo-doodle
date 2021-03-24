fca1744f1de387693a77f0ea6d77367da04772a6
es3017 <78827803+es3017@users.noreply.github.com>
2021-03-23 21:54:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+var wrap = document.body.querySelector(".wrap");
+var counter = 5;
+var data = ["5"];
+var list = [
+  {
+    name: "Harry",
+    age: 25
+  },
+  {
+    name: "Larry",
+    age: 15
+  },
+  {
+    name: "Bert",
+    age: 45
+  },
+  {
+    name: "Jerry",
+    age: 35
+  },
+  {
+    name: "Berry",
+    age: 5
+  },
+  {
+    name: "Bart",
+    age: 55
+  }
+];
+
+
+for (var i = 0; i < list.length; i++) {
+  var ele = document.createElement("div");
+  var eleName = document.createElement("h1");
+  var eleAge = document.createElement("h4");
+
+  if (list[i].age === 25) {
+    ele.style.color = "green";
+  }
+
+  eleName.innerHTML = list[i].name;
+  eleAge.innerHTML = "Age: " + list[i].age;
+  ele.appendChild(eleName);
+  ele.appendChild(eleAge);
+  wrap.appendChild(ele);
+}
+
+for (var i = 5; i < data.length; i++) {
+  createBox(data[i]);
+}
+
+document.body.querySelector(".counter").addEventListener("counter", function () {
+  var textValue = document.body.querySelector(".counter").value;
+  createBox(textValue, true);
+});
